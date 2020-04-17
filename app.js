f24799afcd6d30defc143762929e75c75d0981d8
jonathon lee <jonnyleealas@gmail.com>
2020-04-16 18:32:20 -0700

    10 tigers

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 'use strict';
 var areYouTigerKing = '';
 
+function numberOfTigers() {
+ 
+    var correctAnswer = 10;
+
+    for(var i = 0; i < 8; i = i + 1){
+        var numberGuess = prompt('You are now pregnant with Tigers. 1-11, How many will you have?');
+        if(numberGuess == correctAnswer){
+            alert('10 BRUH! YOU WILL BEAR 10 TIGERS!');
+            break;
+        } else {
+            alert('Come On Bruh!');
+        }
+        if (i == '7') {
+            alert('Damn !');
+            isSithLordOrMaster = 'NO';
+        }
+    }
+}
+
+
+
+
 function dmc (){
     while ((areYouTigerKing != 'NO') && (areYouTigerKing !='YES')){
     areYouTigerKing = prompt('Yes or No Question. Are you a Tiger King?').toUpperCase();
@@ -13,6 +35,8 @@ function dmc (){
    
 
 dmc();
+
+numberOfTigers();
 
 
 
